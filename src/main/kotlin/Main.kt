fef0d81437e05c1c0823b3dce0d96f387a8a4079
2008-21 <ramil.com222@gmail.com>
2023-10-07 10:14:48 +0300

    First

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+import java.util.*
+fun main() {
+    println("Выберите вариант: 0 - Камень, 1 - Ножницы, 2 - Бумага")
+    val userChoice = readlnOrNull()?.toIntOrNull();
+
+    if (userChoice == null || userChoice < 0 || userChoice > 2) {
+        println("Некорректный выбор! Попробуйте еще раз.")
+        return
+    }
+    val computerChoice = (0..2).random()
+    println("Ваш выбор: ${getOptionName(userChoice)}")
+    println("Выбор компьютера: ${getOptionName(computerChoice)}")
+
+    when {
+        userChoice == computerChoice -> println("Ничья!")
+        (userChoice == 0 && computerChoice == 1) || (userChoice == 1 && computerChoice == 2) ||
+                (userChoice == 2 && computerChoice == 0) -> println("Поздравляю, вы победили!")
+        else -> println("Компьютер победил!")
+    }
+}
+
+fun getOptionName(option: Int): String {
+    return when (option) {
+        0 -> "Камень"
+        1 -> "Ножницы"
+        else -> "Бумага"
+    }
+}
